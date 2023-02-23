6b7e24f2942b6527811daf192a309733d07a4268
Matthew Lorber <matthew.lorber@gmail.com>
2023-02-23 09:54:02 -0600

    unit testing #7

--- code.js
+++ code.js
@@ -3,6 +3,6 @@ const helloWorld = function() {
     return "Hello, World!";
 }
 
-function sayHello() {
-    return "Hello, World!";
+function sayHello(input) {
+    return "Hello, " + (input) + "!";
 }
